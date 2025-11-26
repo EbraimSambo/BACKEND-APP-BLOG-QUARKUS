44b5bc7061b92896a5ca45de21eccdcc2170be71
ebraimsambo <ebraimsambo@gmail.com>
2025-11-26 13:21:49 +0000

    feat: Initialize Quarkus project with Maven wrapper and basic REST endpoint
    - Added Maven wrapper script (mvnw.cmd) for easier project setup.
    - Created pom.xml with Quarkus dependencies and configurations.
    - Implemented Dockerfiles for JVM, legacy JAR, native, and micro images.
    - Added GreetingResource class with a simple REST endpoint.
    - Introduced Post entity class for future development.
    - Set up application.properties for configuration.
    - Created integration and unit tests for the GreetingResource.

---
+++ src/main/java/org/acme/features/post/domain/entity/Post.java
@@ -0,0 +1,74 @@
+package org.acme.features.post.domain.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Post {
+    private UUID id;
+    private int internalId;
+    private String title;
+    private String content;
+    private String bannerPath;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public int getInternalId() {
+        return internalId;
+    }
+
+    public void setInternalId(int internalId) {
+        this.internalId = internalId;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public String getBannerPath() {
+        return bannerPath;
+    }
+
+    public void setBannerPath(String bannerPath) {
+        this.bannerPath = bannerPath;
+    }
+
+}
